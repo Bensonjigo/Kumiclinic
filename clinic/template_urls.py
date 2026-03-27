d2cae211232c0aac23e21c3aed7f13c84cfcbc48
Aaronekwaru <aaronekwaru7@gmail.com>
2026-03-27 10:24:46 +0300

    fixing the password issue

--- clinic/template_urls.py
+++ clinic/template_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 from . import template_views
@@ -12,7 +12,7 @@ urlpatterns = [
     path('profile/', template_views.profile_view, name='profile'),
     path('password-change/', auth_views.PasswordChangeView.as_view(
         template_name='clinic/password_change.html',
-        success_url='password_change_done'
+        success_url=reverse_lazy('password_change_done')
     ), name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='clinic/password_change_done.html'
@@ -23,14 +23,14 @@ urlpatterns = [
         template_name='clinic/password_reset.html',
         email_template_name='clinic/password_reset_email.html',
         subject_template_name='clinic/password_reset_subject.txt',
-        success_url='password_reset_done'
+        success_url=reverse_lazy('password_reset_done')
     ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='clinic/password_reset_done.html'
     ), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='clinic/password_reset_confirm.html',
-        success_url='password_reset_complete'
+        success_url=reverse_lazy('password_reset_complete')
     ), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='clinic/password_reset_complete.html'
